247d41b4a8a102ed2895864ce35c1e14b9ae2a89
seong yeong jun <tjddudwns1@naver.com>
2023-12-30 21:12:21 +0900

    [feat] 4-2, 4-3. 데이터베이스 스키마 자동 생성, 필드와 컬럼 매핑

--- src/main/java/com/example/ex1_hello_jpa/domain/member/Member.java
+++ src/main/java/com/example/ex1_hello_jpa/domain/member/Member.java
@@ -1,14 +1,43 @@
 package com.example.ex1_hello_jpa.domain.member;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
 @Getter @Setter
+//@Table(uniqueConstraints = ) unique 제약조건 걸때 씀
 public class Member {
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default ‘EMPTY'")
+    //, unique = true -> 이거는 안 씀, 왜냐? 제약조건 이름이 랜덤으로 되서 에러 발생 시 파악 불가능
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Member() {
+    }
+
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
+        this.id = id;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
+    }
 }
---
+++ src/main/java/com/example/ex1_hello_jpa/domain/member/RoleType.java
@@ -0,0 +1,5 @@
+package com.example.ex1_hello_jpa.domain.member;
+
+public enum RoleType {
+    User, Admin
+}
