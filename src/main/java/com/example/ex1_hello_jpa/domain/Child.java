b63b963b4bcca845c4e26687ffb6f69e5ee30777
seong yeong jun <tjddudwns1@naver.com>
2023-12-31 23:13:51 +0900

    8-3. 영속성 전이(CASCADE))와 고아 객체

---
+++ src/main/java/com/example/ex1_hello_jpa/domain/Child.java
@@ -0,0 +1,24 @@
+package com.example.ex1_hello_jpa.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Child {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CHILD_ID")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Parent parent;
+}
---
+++ src/main/java/com/example/ex1_hello_jpa/domain/Parent.java
@@ -0,0 +1,32 @@
+package com.example.ex1_hello_jpa.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Parent {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PARENT_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Child> childList = new ArrayList<>();
+
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+}
