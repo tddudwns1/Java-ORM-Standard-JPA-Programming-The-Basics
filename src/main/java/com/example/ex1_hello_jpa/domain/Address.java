851b846ac51137b3430fb831c532ded4b409db65
seong yeong jun <tjddudwns1@naver.com>
2024-01-01 12:01:51 +0900

    9-5. 값 타입 컬렉션(값 타입 매핑(사용X))

--- src/main/java/com/example/ex1_hello_jpa/domain/Address.java
+++ src/main/java/com/example/ex1_hello_jpa/domain/Address.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter @Setter
 public class Address {
@@ -11,4 +13,16 @@ public class Address {
     private String street;
     private String zipcode;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
--- src/main/java/com/example/ex1_hello_jpa/domain/Member.java
+++ src/main/java/com/example/ex1_hello_jpa/domain/Member.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -43,6 +45,17 @@ public class Member extends BaseEntity{
 
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     //@Embedded
     //@AttributeOverrides() // 겹치는 임베디드 타입을 사용할 때 구분하기 위해서 사용
